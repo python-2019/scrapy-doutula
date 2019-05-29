2aeb90779f5ed8cd2a1f6112f0eedeb76e60cd5d
shanmu2020 <854731306@QQ.com>
2019-05-29 18:50:29 +0800

    忽略文件

---
+++ doutula/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoutulaItem(scrapy.Item):
+    # define the fields for your item here like:
+    image_list = scrapy.Field()
+    image_path = scrapy.Field()  # 图片本地存储路径(相对路径)
